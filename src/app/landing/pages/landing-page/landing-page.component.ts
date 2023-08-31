36b3875669cab9d6c16f8192b1a64af46454f46c
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-08-31 14:55:51 -0400

    improvement: dynamic navbar

--- src/app/landing/pages/landing-page/landing-page.component.ts
+++ src/app/landing/pages/landing-page/landing-page.component.ts
@@ -5,13 +5,4 @@ import { AuthenticationService } from 'src/app/shared/authentication.service';
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
 })
-export class LandingPageComponent {
-  private authService = inject(AuthenticationService);
-
-  public isUserLoggedIn = this.authService.isUserLoggedIn();
-
-  public logout = () => {
-    this.authService.logout();
-    this.isUserLoggedIn = false;
-  };
-}
+export class LandingPageComponent {}
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { AuthenticationService } from '../../authentication.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
-  constructor(private router: Router) {}
+  private router = inject(Router);
+  private authService = inject(AuthenticationService);
 
   private readonly WHITE = '#EFE9F4';
   private readonly DARK_GREEN = '#082022';
@@ -14,12 +16,14 @@ export class NavbarComponent {
   public pathname: string = '';
   public showAuthLinks: boolean = true;
   public titleColor: string = this.DARK_GREEN;
+  public isUserLoggedIn = this.authService.isUserLoggedIn();
 
   ngOnInit() {
     this.router.events.subscribe((event) => {
       if (!(event instanceof NavigationEnd)) return;
 
       this.updatePathname(event.url);
+      this.isUserLoggedIn = this.authService.isUserLoggedIn();
     });
   }
 
@@ -38,5 +42,10 @@ export class NavbarComponent {
     } else {
       this.titleColor = this.WHITE;
     }
+  };
+
+  public logout = () => {
+    this.authService.logout();
+    this.isUserLoggedIn = false;
   };
 }
